d9089e06b9ee51f46f57916a6c2cf40ef3b6c056
elifcetinkaya <eliifctnk@gmail.com>
2022-11-12 11:52:07 +0300

    feature: sidebar design

--- BYS/fe/src/components/home/sidebar/index.js
+++ BYS/fe/src/components/home/sidebar/index.js
@@ -1,8 +1,39 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import { Typography, Button, Grid } from "@mui/material";
 
 export default function SiderBar() {
   return (
-    <Container fixed sx={{ background: "#fff", height: "100vh" }}></Container>
+    <Container
+      fixed
+      sx={{
+        background: "#fff",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      {" "}
+      <Typography variant="h3" component="h2">
+        Belge Yönetim Sistemi
+      </Typography>
+      <Typography variant="h5" sx={{ mt: 2 }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt.
+      </Typography>
+      <Typography sx={{ mt: 2 }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam.
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ mt: 4, width: "400px", mx: "auto" }}
+        size="large"
+      >
+        E-Devlet ile Giriş Yap
+      </Button>
+    </Container>
   );
 }
