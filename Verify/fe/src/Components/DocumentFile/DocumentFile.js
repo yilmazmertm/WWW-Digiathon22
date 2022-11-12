cd3f04ad09653f41f13cb0490c7ae81153bcf295
Ahmet <mrkronoss@hotmail.com>
2022-11-12 20:08:48 +0300

    fix : verify

---
+++ Verify/fe/src/Components/DocumentFile/DocumentFile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DocumentFile() {
+  return (
+    <div>DocumentFile</div>
+  )
+}
---
+++ Verify/fe/src/Components/DocumentNo/DocumentNo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DocumentNo() {
+  return (
+    <div>DocumentNo</div>
+  )
+}
