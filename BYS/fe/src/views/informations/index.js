f7fcf7508e395f1b04ada79649fbd39b43724b9c
elifcetinkaya <eliifctnk@gmail.com>
2022-11-12 20:02:06 +0300

    fix: css

--- BYS/fe/src/views/informations/index.js
+++ BYS/fe/src/views/informations/index.js
@@ -4,7 +4,7 @@ import { Grid } from "@mui/material";
 
 export default function Informations() {
   return (
-    <Grid container md={8} mx="auto">
+    <Grid container mx="auto">
       <Grid item xs={12}>
         <Form />
       </Grid>
