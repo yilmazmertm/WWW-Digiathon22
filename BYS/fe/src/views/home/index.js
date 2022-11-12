4bac0e73284878ca91693c192c9c2800d0e00375
elifcetinkaya <eliifctnk@gmail.com>
2022-11-12 11:15:59 +0300

    feature: home page and components

---
+++ BYS/fe/src/views/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SiderBar from "../../components/home/sidebar";
+
+export default function Home() {
+  return (
+    <div>
+      <SiderBar />
+    </div>
+  );
+}
