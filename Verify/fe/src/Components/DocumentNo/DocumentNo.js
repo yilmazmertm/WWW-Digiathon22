915d18ebda6eb37c5ab1b197abdb3a9329c3b998
elifcetinkaya <eliifctnk@gmail.com>
2022-11-12 20:41:03 +0300

    feature: verify doc inputs

--- Verify/fe/src/Components/DocumentNo/DocumentNo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DocumentNo() {
-  return (
-    <div>DocumentNo</div>
-  )
-}
--- Verify/fe/src/View/DocumentVerify.js
+++ Verify/fe/src/View/DocumentVerify.js
@@ -1,11 +1,9 @@
 import React from "react";
 import DocumentFile from "../Components/DocumentFile/DocumentFile";
-import DocumentNo from "../Components/DocumentNo/DocumentNo";
 export default function DocumentVerify() {
   return (
     <div>
       <DocumentFile />
-      <DocumentNo />
     </div>
   );
 }
