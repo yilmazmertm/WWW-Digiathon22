cc5ed535dcc0cd82204506df566533e98729eed5
Mert YILMAZ <yilmazmertm@gmail.com>
2022-11-12 23:39:39 +0300

    sc final

--- sc/BYS.sol
+++ sc/BYS.sol
@@ -1,44 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BYS is Ownable {
+
     mapping(address => bool) private _issuers;
     mapping(bytes32 => address) private _isRegisteredDocHash;
     mapping(string => bytes32) private _isRegisteredDoc;
-    
+
+    event Register(address indexed issuer);
+    event Revoke(address indexed issuer);
+    event RegisterDocument(address indexed registrar, bytes32 docHash, string documentId);
+
     modifier onlyIssuer() {
-        require(_issuers[msg.sender] == true, "Not a valid sender");
+        require(_issuers[msg.sender] == true,"Not a valid issuer");
         _;
     }
 
-    modifier onlyNonRegisteredDoc(bytes32 docHash, string memory documentId) {
+    modifier onlyNonRegisteredDoc(bytes32 docHash, string calldata documentId) {
         require(_isRegisteredDocHash[docHash] == address(0));
         require(_isRegisteredDoc[documentId] == bytes32(0));
         _;
     }
 
-    function register(address _issuer) public onlyOwner {
-        _issuers[_issuer] = true;
+    
+    function register(address issuer) public onlyOwner {
+        _issuers[issuer] = true;
+        emit Register(issuer);
     }
 
-    function revoke(address _issuer) public onlyOwner {
-        _issuers[_issuer] = false;
+    
+    function revoke(address issuer) public onlyOwner {
+        _issuers[issuer] = false;
+        emit Revoke(issuer);
     }
 
-    function isIssued(address caller) public view returns (bool) {
-        return _issuers[caller];
+    function registerDocument(bytes32 docHash, string calldata documentId) public 
+        onlyIssuer onlyNonRegisteredDoc(docHash, documentId) {
+        _isRegisteredDocHash[docHash] = msg.sender;
+        _isRegisteredDoc[documentId] = docHash;
     }
 
-    function registerDocument(bytes32 docHash, string memory documentId) public onlyIssuer onlyNonRegisteredDoc(docHash, documentId) {
-      _isRegisteredDocHash[docHash] = msg.sender;
-      _isRegisteredDoc[documentId] = docHash;
-    }
 
-    function isRegisteredDocument(bytes32 docHash, string memory documentId) public view returns (address){
-        require( docHash != bytes32(0));
-        require(_isRegisteredDoc[documentId] == docHash, "Incorrect or non registered doc id");
+    function isRegisteredDocument(bytes32 docHash, string memory documentId) 
+        public view returns (address) {
+        require(docHash != bytes32(0));
+        require(_isRegisteredDoc[documentId] == docHash,"Incorrect or non registered doc ID");
         return _isRegisteredDocHash[docHash];
     }
 }
