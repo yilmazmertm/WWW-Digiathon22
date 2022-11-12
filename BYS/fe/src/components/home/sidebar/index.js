7b0f514df828a2204d2286372c3248aa0da66513
elifcetinkaya <eliifctnk@gmail.com>
2022-11-12 11:31:56 +0300

    feature: mui

--- BYS/fe/src/components/home/sidebar/index.js
+++ BYS/fe/src/components/home/sidebar/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Container from "@mui/material/Container";
 
 export default function SiderBar() {
-  return <div>index</div>;
+  return (
+    <Container fixed sx={{ background: "#fff", height: "100vh" }}></Container>
+  );
 }
--- BYS/fe/src/views/home/index.js
+++ BYS/fe/src/views/home/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import SiderBar from "../../components/home/sidebar";
+import List from "../../components/home/list";
+import Grid from "@mui/material/Grid";
 
 export default function Home() {
   return (
-    <div>
-      <SiderBar />
-    </div>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      <Grid item xs={6}>
+        <SiderBar />
+      </Grid>
+      <Grid item xs={6}>
+        <List />
+      </Grid>
+    </Grid>
   );
 }
