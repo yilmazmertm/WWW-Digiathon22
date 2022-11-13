afe4783c4c7c01b1206f8ee8dc7cfbf2414bd178
elifcetinkaya <eliifctnk@gmail.com>
2022-11-13 09:45:07 +0300

    fix: css

--- Portal/fe/src/views/createPDF/index.js
+++ Portal/fe/src/views/createPDF/index.js
@@ -5,9 +5,19 @@ export default function CreatePdf() {
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
   return (
-    <div>
-      <Form setName={setName} setSurname={setSurname} />
-      <DocumentPDF name={name} surname={surname} />
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          width: "600px",
+          height: "100vh",
+          background:
+            "linear-gradient(267.27deg, #3f3e50 1.57%, #157d5f 98.69%)",
+        }}
+      ></div>
+      <div style={{marginLeft:"60px"}}>
+        <Form setName={setName} setSurname={setSurname} />
+        <DocumentPDF name={name} surname={surname} />
+      </div>
     </div>
   );
 }
